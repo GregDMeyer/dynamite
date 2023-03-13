020c0d0d95d884bc035d11b01cf1f98056a7ecfe
Greg Meyer <gregory.meyer@gmail.com>
2023-03-13 11:39:52 -0700

    lattice diagrams from basis vectors working

--- examples/scripts/kagome/lattice_library.py
+++ examples/scripts/kagome/lattice_library.py
@@ -1,4 +1,7 @@
 
+import numpy as np
+
+
 lattice_diagrams = {
 ######
 'L6' : '''
@@ -106,6 +109,59 @@ def print_index_map(vertices):
                 col += min(len(str(idx)), 2)
 
         print()
+
+
+def diagram_from_basis(a, b):
+    # "a" should always be the one clockwise
+    if cross_product(a, b) < 0:
+        a, b = b, a
+
+    # they use unit cells of length 2, here it will be more convenient to just use units
+    a = (2*a[0], 2*a[1])
+    b = (2*b[0], 2*b[1])
+
+    len_x = abs(a[0]) + abs(b[0])
+    len_y = abs(a[1]) + abs(b[1])
+
+    origin_x = abs(min(0, a[0], b[0]))
+    origin_y = abs(min(0, a[1], b[1]))
+
+    lattice = np.ones((len_x+1, len_y+1))
+
+    # turn it from a triangular lattice into kagome
+    lattice[1::2, 0::2] = 0
+
+    # now delete everything that is outside of the parallelogram
+    # specified by a and b
+    corners = [np.array(v) for v in [(0, 0), a, (a[0]+b[0], a[1]+b[1]), b]]
+
+    for x in range(len_x+1):
+        for y in range(len_y+1):
+            # check that point is "on the correct side" of each line
+            for i, j in [(0, 1), (1, 2), (2, 3), (3, 0)]:
+                side_vec = corners[j] - corners[i]
+                point_vec = np.array([x-origin_x, y-origin_y]) - corners[i]
+                if cross_product(side_vec, point_vec) < 0:
+                    lattice[x, y] = 0
+
+    diagram_rows = []
+    for y in range(len_y+1):
+        diagram_rows.append(y*' ')
+        for x in range(len_x+1):
+            if lattice[x, y]:
+                diagram_rows[-1] += 'o '
+            else:
+                diagram_rows[-1] += '  '
+
+    # get rid of leading spaces, and form a single string
+    to_trim = min(len(s) - len(s.lstrip(' ')) for s in diagram_rows)
+    rtn = '\n'.join(row[to_trim:] for row in diagram_rows[::-1])
+
+    return rtn
+
+
+def cross_product(a, b):
+    return a[0]*b[1] - a[1]*b[0]
 
 
 def main():
